886ed584d6efd15647d258ee3642dc27b1a51e78
Max <maxime-obry@hotmail.fr>
2021-12-02 11:49:06 +0100

    1st vommit

--- menu.go
+++ menu.go
@@ -6,8 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"time"
 	"strings"
+	"time"
 )
 
 func Clear() {
@@ -96,56 +96,14 @@ func Readword3() {
 }
 
 func wordToUnderscore(motChoisi string) string {
-    str2 := strings.ReplaceAll(motChoisi, "[a-zA-Z]", "_"); // remplace chaque lettre "_"
+	str2 := strings.ReplaceAll(motChoisi, "[a-zA-Z]", "_") // remplace chaque lettre "_"
 	return str2
 }
 
-func addLetter(hidden string) string {
-	var  string
-}
-	
-
 func Guesstheword() {
 
-		INletter := bufio.NewScanner(os.Stdin)
-		INletter.Scan()
-		letter := Capitalize(INletter.Text())
-	}
-	
-	func IsLetter2(r rune) bool {
-		lettermaj := ('A' <= r) && (r <= 'Z')
-		lettermin := ('a' <= r) && (r <= 'z')
-		letternum := ('0' <= r) && (r <= '9')
-		if lettermaj || lettermin || letternum {
-			return true
-		}
-		return false
-	}
-	
-	func IsMin(r rune) bool {
-		lettermin := ('a' <= r) && (r <= 'z')
-		return lettermin
-	}
-	
-	func IsMaj(r rune) bool {
-		lettermaj := ('A' <= r) && (r <= 'Z')
-		return lettermaj
-	}
-	
-	func Capitalize(s string) string {
-		str2 := []rune(s)
-		for index := range str2 {
-			if index == 0 {
-				if IsMin(str2[index]) {
-					str2[index] -= 32
-				}
-			} else if !IsLetter2(str2[index-1]) && IsMin(str2[index]) {
-				str2[index] -= 32
-			} else if IsLetter2(str2[index-1]) && IsMaj(str2[index]) {
-				str2[index] += 32
-			}
-		}
-		return string(str2)
-	}
-		for_, motChoisi := range 
+	INletter := bufio.NewScanner(os.Stdin)
+	INletter.Scan()
+
+
 }
