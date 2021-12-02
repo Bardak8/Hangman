1470a3caefdb4f1eb4e5ad77633c95c535058e1d
Lunasphys <laurie.jeham@hotmail.fr>
2021-12-02 11:48:46 +0100

    remplacer underscore

---
+++ capitalize.go
@@ -0,0 +1,39 @@
+package main
+
+
+
+func IsLetter2(r rune) bool {
+	lettermaj := ('A' <= r) && (r <= 'Z')
+	lettermin := ('a' <= r) && (r <= 'z')
+	letternum := ('0' <= r) && (r <= '9')
+	if lettermaj || lettermin || letternum {
+		return true
+	}
+	return false
+}
+
+func IsMin(r rune) bool {
+	lettermin := ('a' <= r) && (r <= 'z')
+	return lettermin
+}
+
+func IsMaj(r rune) bool {
+	lettermaj := ('A' <= r) && (r <= 'Z')
+	return lettermaj
+}
+
+func Capitalize(s string) string {
+	str2 := []rune(s)
+	for index := range str2 {
+		if index == 0 {
+			if IsMin(str2[index]) {
+				str2[index] -= 32
+			}
+		} else if !IsLetter2(str2[index-1]) && IsMin(str2[index]) {
+			str2[index] -= 32
+		} else if IsLetter2(str2[index-1]) && IsMaj(str2[index]) {
+			str2[index] += 32
+		}
+	}
+	return string(str2)
+}
