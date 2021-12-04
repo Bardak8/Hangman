fa09567abd58466cd92c4437a61f8889da0c585d
Lunasphys <laurie.jeham@hotmail.fr>
2021-12-04 15:40:01 +0100

    Fonction Readword ok

--- menu.go
+++ menu.go
@@ -58,23 +58,27 @@ func debut() {
 	o := scanner.Text()
 	switch o {
 	case "1":
-		Readword1()
+		startGame("words.txt", 37)
 	case "2":
-		Readword2()
+		startGame("words2.txt", 23)
 	case "3":
-		Readword3()
+		startGame("words3.txt", 24)
 	}
 }
 
-func Readword1() string {
+func startGame(filename string, nbword int) {
+	Readword(filename, nbword)
+}
+
+func Readword(filename string, nbword int) string {
 	rand.Seed(time.Now().UnixNano())
-	randnumber := (rand.Intn(37))
+	randnumber := (rand.Intn(nbword))
 	// Open the file.
-    f, _ := os.Open("words.txt")
+    f, _ := os.Open(filename)
     // Create a new Scanner for the file.
     scanner := bufio.NewScanner(f)
     // Loop over all lines in the file and print them.
-	index := 0
+	index := 1
     for scanner.Scan() {
       line := scanner.Text()
 	  index++
@@ -85,21 +89,12 @@ func Readword1() string {
 	return ""
 }
 
-func Readword2() {
-
-	// file, _ := os.Open("words2.txt")
-	// scanner := bufio.NewScanner(file)
-}
-func Readword3() {
-
-	// file, _ := os.Open("words3.txt")
-	// scanner := bufio.NewScanner(file)
-}
 
 func wordToUnderscore(motChoisi string) string {
 	str2 := strings.ReplaceAll(motChoisi, "[a-zA-Z]", "_") // remplace chaque lettre "_"
 	return str2
 }
+
 
 // func replace(hiddenword string, motChoisi string) string {
 // 	INletter := bufio.NewScanner(os.Stdin)
