97360ca1bab96a7e40c2e3be1e54ed3a5c3ac4b9
Lunasphys <laurie.jeham@hotmail.fr>
2021-12-04 15:33:14 +0100

    function readwords finish

--- menu.go
+++ menu.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"math/rand"
+
+	// "math/rand"
 	"os"
 	"strings"
 	"time"
@@ -23,7 +25,7 @@ func SlowPrint(str ...string) {
 }
 
 func start() {
-	SlowPrint("Bonjour ")
+	SlowPrint("Bonjour \n")
 	fmt.Println("1 = Démarrer l'éxécution")
 	fmt.Println("2 = Non, je ne souhaite tuer personne")
 	// créer une var scanner qui va lire ce que l'utilisateur va écrire
@@ -65,25 +67,33 @@ func debut() {
 }
 
 func Readword1() string {
-	var word string
-	file, _ := os.Open("words.txt")
-	scanner := bufio.NewScanner(file)
 	rand.Seed(time.Now().UnixNano())
-	(rand.Intn(30))
-	for scanner.Text() {
-		fmt.println("azejazoejzpajejpzeporjzerozreozro")
-	}
-
+	randnumber := (rand.Intn(37))
+	// Open the file.
+    f, _ := os.Open("words.txt")
+    // Create a new Scanner for the file.
+    scanner := bufio.NewScanner(f)
+    // Loop over all lines in the file and print them.
+	index := 0
+    for scanner.Scan() {
+      line := scanner.Text()
+	  index++
+	  if index == randnumber {
+		  return line
+	  }
+    }
+	return ""
 }
+
 func Readword2() {
 
-	file, _ := os.Open("words2.txt")
-	scanner := bufio.NewScanner(file)
+	// file, _ := os.Open("words2.txt")
+	// scanner := bufio.NewScanner(file)
 }
 func Readword3() {
 
-	file, _ := os.Open("words3.txt")
-	scanner := bufio.NewScanner(file)
+	// file, _ := os.Open("words3.txt")
+	// scanner := bufio.NewScanner(file)
 }
 
 func wordToUnderscore(motChoisi string) string {
@@ -91,54 +101,32 @@ func wordToUnderscore(motChoisi string) string {
 	return str2
 }
 
-func replace(hiddenword string, motChoisi string) string {
-	INletter := bufio.NewScanner(os.Stdin)
-	INletter.Scan()
-<<<<<<< HEAD
-	i := findIndex(hiddenword, motChoisi)
-	var res string
-	str1 := hiddenword[i]
-	str2 := motChoisi[i]
-	str3 := []rune(hiddenword)
-	for i, lettre := range hiddenword {
-		if i >= 0 {
-			res == (str3-str1)+str2
-		} else {
-			nil
-		}
-=======
-	{
+// func replace(hiddenword string, motChoisi string) string {
+// 	INletter := bufio.NewScanner(os.Stdin)
+// 	INletter.Scan()
+// 	i := findIndex(hiddenword, motChoisi)
+// 	var res string
+// 	str1 := hiddenword[i]
+// 	str2 := motChoisi[i]
+// 	str3 := []rune(hiddenword)
+// 	for i, lettre := range hiddenword {
+// 		if i >= 0 {
+// 			res == (str3-str1)+str2
+// 		} else {
+// 			nil
+// 		}
+// 	}
+// 	return res
+// }
 
->>>>>>> d02e62277dc72f5bebda56994116419a379ccd5f
-	}
-	return res
-}
-
-func findIndex(hiddenword string, motChoisi string) int {
-<<<<<<< HEAD
-	var lettre bool
-	for i, letter := range motChoisi {
-		lettre = Contains(motChoisi, rune(letter))
-		if lettre == false {
-			fmt.Print("raté")
-		}
-		return i
-	}
-	return -1
-}
-=======
-
-	var lettre bool
-	for i, letter := range motChoisi {
-		lettre = Contains(motChoisi, rune(letter))
-		if lettre == true {
-			return i
-		}
-	}
-	return 0
-}
-
-func baba() {
-
-}
->>>>>>> d02e62277dc72f5bebda56994116419a379ccd5f
+// func findIndex(hiddenword string, motChoisi string) int {
+// 	var lettre bool
+// 	for i, letter := range motChoisi {
+// 		lettre = Contains(motChoisi, rune(letter))
+// 		if lettre == false {
+// 			fmt.Print("raté")
+// 		}
+// 		return i
+// 	}
+// 	return -1
+// }
