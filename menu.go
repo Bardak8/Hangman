5d1c6e606656c5babcfb4d47445455d0501954f8
Max <maxime-obry@hotmail.fr>
2021-12-09 09:04:36 +0100

    endofhangman

--- menu.go
+++ menu.go
@@ -87,23 +87,12 @@ func startGame(filename string, nbword int) {
 	}
 	// trouve le mot et transforme le mot choisi en underscore
 }
-func Readword(filename string, nbword int) string {
-	rand.Seed(time.Now().UnixNano())
-	randnumber := (rand.Intn(nbword))
-	// Open the file.
-	f, _ := os.Open(filename)
-	// Create a new Scanner for the file.
-	scanner := bufio.NewScanner(f)
-	// Loop over all lines in the file and print them.
-	index := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		if index == randnumber {
-			return line
-		}
-		index++
+func Readword(filename string) string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Error when opening file: %s", err)
 	}
-	return ""
+	for  
 }
 
 func wordToUnderscore() string {
